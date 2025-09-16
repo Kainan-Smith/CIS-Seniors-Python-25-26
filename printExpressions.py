07804024a264d269c4aa3f86e91da4eb75f7112f
Kainan :] <cte-kainan.smith@sllboces.org>
2025-09-16 13:53:06 +0000

    The beables

---
+++ printExpressions.py
@@ -0,0 +1,18 @@
+'''
+Class: CIS Seniors 25-26
+Name: Kainan Smith
+Assignment: Printing Expressions
+'''
+
+print("Hello, My name is Kainan")
+
+print("My favorite song!")
+print("-----------------")
+print()
+print("Yesterday, all my troubles seemed so far away.")
+print("Now it looks as though they're here to stay.")
+print("Oh I believe in yesterday.")
+print("Suddenly, I'm not half the man I'm used to be.")
+print("There's a shadow hanging over me, oh yesterday came suddenly.")
+print("Why she had to go I don't know, she wouldn't say.")
+print("I said something wrong now I long for yesterday.")
