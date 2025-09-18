f60f2068988fb22bc8bef6135fb752a34c7bbdd2
Kainan :] <cte-kainan.smith@sllboces.org>
2025-09-18 14:37:41 +0000

    Thursday finished

---
+++ Unit 1:/socialProfile.py
@@ -0,0 +1,31 @@
+'''
+File: socialProfile.py
+Name: Kainan Smith
+Course: Computer and Information Science
+Date: 9/18/2025
+'''
+# Strings
+username = "kenoSecretary"
+bio = "I like to play and draw"
+favorite_quote = "Buy my album"
+# Integers
+followers = 15
+following = 120
+posts = 6
+# Floats
+average_like_per_post = 2.8
+engagement_rate = 4.5
+# Booleans
+verified_account = False
+private_profile = True
+
+print("Username:", username)
+print("Bio:", bio)
+print("Favorite Quote:", favorite_quote)
+print("Followers:", followers)
+print("Following:", following)
+print("Posts:", posts)
+print("Likes per post:", average_like_per_post)
+print("Engagement Rate:", engagement_rate)
+print("Verified?:", verified_account)
+print("Private?:", private_profile)
