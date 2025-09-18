c6b32e2eb6742611cdd2c658c9e02f18013aa6e4
Kainan :] <cte-kainan.smith@sllboces.org>
2025-09-18 13:27:12 +0000

    Thursday dreamVacation.py

---
+++ dreamVacation.py
@@ -0,0 +1,17 @@
+'''
+File: Dream Vacation
+Name: Kainan Smith
+Course: Computer and Information Science
+Date: 9/18/2025
+'''
+
+# Assigning Variables
+location = "Las Vegas"
+duration = 4
+activity = "Watching a tournament"
+travelBuddies = "Xaiden, Youssef, and Zayan"
+
+print("My dream vacation spot is", location + ".")
+print("I would like to go for", duration, "days.")
+print("In", location, "we would be", activity + ".")
+print("I would like to go with", travelBuddies + ".")
