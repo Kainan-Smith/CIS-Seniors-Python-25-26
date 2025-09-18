5ae54b677c135d321585562445ec2d98f3cb8953
Kainan :] <cte-kainan.smith@sllboces.org>
2025-09-18 13:17:35 +0000

    Thursday morePyVars.py

---
+++ morePyVars.py
@@ -0,0 +1,35 @@
+'''
+The Doc String
+Description: Notes about Python variables
+Name: Kainan Smith
+Date: 9/18/2025
+'''
+print("\n\n\n")
+# Variable Assignment
+firstName = "Kainan"
+lastName = "Smith"
+gradeLevel = 17
+favoriteSubject = "Music"
+yearsInSchool = 12
+
+print("My name is", firstName, lastName + ".")
+print("I am in", str(gradeLevel) + "th grade.")
+print("My favorite subject is", favoriteSubject + ".")
+print("I have been in school for", str(yearsInSchool), "years.")
+
+print("\n\n\n")
+
+print("Mathematical calculations:")
+
+# Storing mathematical values
+length = 15
+width = 8
+height = 12
+
+# Calculate area and volume and store them in variables
+area = length * width
+volume = area * height
+
+# Print out the values
+print("The area of our structure is:", str(area) + ".")
+print("The volume of our structure is:", str(volume) + ".")
