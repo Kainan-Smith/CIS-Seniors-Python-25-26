dd8e62b844c0108f73b48c381804c8f5a99c80c5
Kainan :] <cte-kainan.smith@sllboces.org>
2025-10-09 14:51:53 +0000

    variableValidator

--- Projects and Quizzes/variableValidator.py
+++ Projects and Quizzes/variableValidator.py
@@ -8,28 +8,49 @@ pythonKeywords = [
     'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
     'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
     'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try',
-    'while', 'with', 'yield'
+    'while', 'with', 'yield', 'print'
 ]
-numbers = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
-]
-
 # Display welcome message
 print("=== PYTHON VARIABLE NAME VALIDATOR ===")
 print("This program checks if your variable name is valid in Python.")
 print()
 
-# Get user input
-variableName = input("Enter a variable name to validate: ")
-varNameChars = list(variableName)
-
-
-# Your validation code goes here
-for item in (varNameChars):
-    if variableName == "" or variableName == " ":
-        print("Invalid Variable Name.  Identifier cannot be empty.")
-    elif varNameChars[0] in numbers:
-        print("Invalid Variable Name.  Identifier cannot start with a number.")
-        break
+validVar = True
+specialChar = False
+emptyString = False
+startNumber = False
+pyKeyword = False
 
 # Check each rule and provide appropriate feedback
+while True:
+    variableName = input("Enter a variable name to validate: ")
+    varNameChars = list(variableName)
+    firstChar = varNameChars[0]
+    wrongChar = variableName.isalnum()
+
+    if variableName == "":
+        emptyString = True
+        validVar = False
+    if wrongChar == False:
+        specialChar = True
+        validVar = False
+    if firstChar.isdigit():
+        startNumber = True
+        validVar = False
+    if variableName in pythonKeywords:
+        pyKeyword = True
+        validVar = False
+
+    if validVar == False:
+        print("Invalid Variable Name.")
+        if specialChar == True:
+            print("Identifier cannot contain spaces or special characters other than \"_\".")
+        if emptyString == True:
+            print("Identifier cannot be empty.")
+        if startNumber == True:
+            print("Identifier cannot start with a number.")
+        if pyKeyword == True:
+            print("Identifier cannot be the name of a Python Keyword.")
+    else:
+        print("Valid Variable Name.  Good Job!")
+        break
